69d6ae860e5e105f49a73458a09a859f0e907f24
manjakarak <manjaka.rakoto@myagency.mg>
2022-09-29 16:56:02 +0300

    add some dep

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -20,58 +20,61 @@
     </div>
 </template>
 <script setup lang="ts">
-import UserService from '@/services/userService';
-import store from '@/store';
-import emailValidation from '@/utils/validation/email_validation';
-import { onMounted, reactive, ref } from 'vue';
-import { useRoute, useRouter } from 'vue-router';
+    import UserService from '@/services/userService';
+    import store from '@/store';
+    import emailValidation from '@/utils/validation/email_validation';
+    import { onMounted, reactive, ref } from 'vue';
+    import { useRoute, useRouter } from 'vue-router';
 
-// route
-const route = useRoute();
-const router = useRouter();
+    // route
+    const route = useRoute();
+    const router = useRouter();
 
-// ref
-const isEmailValid = ref<boolean>(false);
-const isFormValid = ref<boolean>(false);
-let formParams = reactive<{ username: string; password: string }>({
-    username: '',
-    password: '',
-});
+    // ref
+    const isEmailValid = ref<boolean>(false);
+    const isFormValid = ref<boolean>(false);
+    let formParams = reactive<{ username: string; password: string }>({
+        username: '',
+        password: '',
+    });
 
-// fn
-const handleInput = (e: Event) => {
-    const name: string = (e.target as HTMLInputElement).name;
-    const value: string = (e.target as HTMLInputElement).value;
-    switch (name) {
-        case 'username':
-            isEmailValid.value = emailValidation(value);
-            break;
-        default:
-            break;
-    }
-    formParams = {
-        ...formParams,
-        [name]: value,
+    // fn
+    const handleInput = (e: Event) => {
+        const name: string = (e.target as HTMLInputElement).name;
+        const value: string = (e.target as HTMLInputElement).value;
+        switch (name) {
+            case 'username':
+                isEmailValid.value = emailValidation(value);
+                break;
+            default:
+                break;
+        }
+        formParams = {
+            ...formParams,
+            [name]: value,
+        };
     };
-};
 
-const handleUserLogin = async (): Promise<void> => {
-    try {
-        const { token, user } = await UserService.login(formParams);
-        store.dispatch('UserModule/setUserDetails', { detail: user, token });
-        const redirectTo = route.query.redirect?.toString();
-        redirectTo ? router.push(redirectTo) : router.push('/');
-    } catch (error) {
-        console.log(error);
-    }
-};
+    const handleUserLogin = async (): Promise<void> => {
+        try {
+            const { token, user } = await UserService.login(formParams);
+            store.dispatch('UserModule/setUserDetails', {
+                detail: user,
+                token,
+            });
+            const redirectTo = route.query.redirect?.toString();
+            redirectTo ? router.push(redirectTo) : router.push('/');
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
-const handleSubmit = () => {
-    const isDataValid: Boolean = Object.values(formParams).every(
-        (value) => value !== ''
-    );
-    isFormValid.value = isDataValid && isEmailValid.value ? true : false;
-    isFormValid.value === true ? handleUserLogin() : console.log('not ok');
-};
+    const handleSubmit = () => {
+        const isDataValid: Boolean = Object.values(formParams).every(
+            (value) => value !== ''
+        );
+        isFormValid.value = isDataValid && isEmailValid.value ? true : false;
+        isFormValid.value === true ? handleUserLogin() : console.log('not ok');
+    };
 </script>
 <style lang=""></style>
