a757d5bbe5e7e0c6bb8fc8e661b305eadb32b7fd
manjakarak <manjaka.rakoto@myagency.mg>
2022-09-30 17:23:44 +0300

    etab validation handling on registration page

--- src/pages/registration/Registration.vue
+++ src/pages/registration/Registration.vue
@@ -1,42 +1,90 @@
 <template>
     <div>
         <form @submit.prevent="handleSubmit" action="post">
-            <input @input="handleInput" type="text" name="name" id="name" />
-            <input @input="handleInput" type="text" name="email" id="email" />
-            <input @input="handleInput" type="text" name="logo" id="logo" />
-            <input @input="handleInput" type="text" name="adress" id="adress" />
             <input
+                placeholder="Nom"
+                @input="handleInput"
+                type="text"
+                name="name"
+                id="name"
+                :value="registerData.name"
+            />
+            <input
+                placeholder="email"
+                @input="handleInput"
+                type="text"
+                name="email"
+                id="email"
+                :value="registerData.email"
+            />
+            <input
+                placeholder="logo"
+                @input="handleInput"
+                type="text"
+                name="logo"
+                id="logo"
+                :value="registerData.logo"
+            />
+            <input
+                placeholder="addresse"
+                @input="handleInput"
+                type="text"
+                name="adress"
+                id="adress"
+                :value="registerData.adress"
+            />
+            <input
+                placeholder="nom inc."
                 @input="handleInput"
                 type="text"
                 name="nameEntreprise"
                 id="nameEntreprise"
+                :value="registerData.nameEntreprise"
             />
             <input
+                placeholder="nb inc."
                 @input="handleInput"
                 type="text"
                 name="numberEntreprise"
                 id="numberEntreprise"
+                :value="registerData.numberEntreprise"
             />
             <input
                 @input="handleInput"
+                placeholder="Mot de passe"
                 type="text"
                 name="password"
                 id="password"
+                :value="registerData.password"
             />
             <input
                 @input="handleInput"
+                placeholder="confirmer mot de passe"
                 type="text"
                 name="confirmPassword"
                 id="confirmPassword"
+                :value="registerData.confirmPassword"
             />
+            <!-- SHOW ERROR -->
+            <p
+                v-if="
+                    isSubmitClicked &&
+                    (isValid?.name === false || !isErrorObjEmpty)
+                "
+            >
+                Nom obligatoire
+            </p>
 
             <button type="submit">Enregister</button>
         </form>
     </div>
 </template>
 <script lang="ts" setup>
-    import { reactive } from 'vue';
-    import useForm from '@/composables/useForm';
+    import useFormValidation from '@/composables/useFormValidation';
+    import { computed } from '@vue/reactivity';
+    import { reactive, ref } from 'vue';
+
+    const { isValid, validateNameField, resetError } = useFormValidation();
     interface IUser {
         name?: string;
         email: string;
@@ -47,6 +95,7 @@
         password: string;
         confirmPassword: string;
     }
+
     let registerData = reactive<IUser>({
         name: '',
         email: '',
@@ -57,11 +106,47 @@
         password: '',
         confirmPassword: '',
     });
+
+    let resetFields = reactive<IUser>({ ...registerData });
+
+    const isSubmitClicked = ref<boolean>(false);
+
+    const isErrorObjEmpty: any = computed(() => {
+        return Object.values(isValid).length;
+    });
+
+    function isPasswordConfirmed(): boolean {
+        return (
+            registerData?.password !== '' &&
+            registerData?.confirmPassword !== '' &&
+            registerData?.password === registerData?.confirmPassword
+        );
+    }
+
     function handleInput(e: Event) {
         const name: string = (e.target as HTMLInputElement).name;
         const value: string = (e.target as HTMLInputElement).value;
+        validateNameField(name, value);
         registerData = { ...registerData, [name]: value };
-        console.log(registerData);
     }
-    function handleSubmit() {}
+
+    function sendFormData() {
+        console.log('call API');
+        registerData = { ...resetFields };
+    }
+    function handleSubmit() {
+        isSubmitClicked.value = true;
+        const requiedFields = [
+            'email',
+            'adress',
+            'password',
+            'confirmPassword',
+        ] as Array<string>;
+        let isFormFieldsNotEmpty: boolean = false;
+        requiedFields.forEach((key) => {
+            isFormFieldsNotEmpty = isValid[key] === true ? true : false;
+        });
+
+        isFormFieldsNotEmpty && isPasswordConfirmed() && sendFormData();
+    }
 </script>
