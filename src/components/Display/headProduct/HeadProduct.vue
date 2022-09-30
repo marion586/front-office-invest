cbc2e0c9a27cf3c97d21ce76a7b2bf335d5191d1
marion586 <marionmenye@gmail.com>
2022-09-30 17:44:53 +0300

    create head product

---
+++ src/components/Display/headProduct/HeadProduct.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="container-head-product">
+        <div class="wrapper">
+            <avatar src="https://joeschmoe.io/api/v1/random" :size="40" />
+            <div class="content">
+                <Title type="h4" :label="label" weight="bold" />
+                <label>{{ Text }}</label>
+            </div>
+        </div>
+        <div class="heart-icon">
+            <span>H</span>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import Title from '../../Common/Title/Title.vue';
+    import avatar from 'ant-design-vue/lib/avatar';
+    defineProps({
+        label: {
+            type: String,
+            required: true,
+        },
+        Text: {
+            type: String,
+            required: true,
+        },
+    });
+</script>
+
+<style lang="scss">
+    .container-head-product {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        .wrapper {
+            display: flex;
+            gap: 25px;
+            align-items: center;
+            .content {
+                display: flex;
+                flex-direction: column;
+                gap: 4px;
+                label {
+                    font-family: 'Montserrat';
+                    font-style: normal;
+                    font-weight: 300;
+                    font-size: 14px;
+                    line-height: 17px;
+                    color: #8f8f8f;
+                }
+            }
+        }
+        .heart-icon {
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+            padding: 10px;
+            gap: 10px;
+
+            width: 38px;
+            height: 38px;
+            background: var(--color-gray);
+            border-radius: 100px;
+        }
+    }
+</style>
---
+++ src/components/Display/productCard/ProductCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container-card">
+        <HeadProduct Text="Agence" label="Label Immo" />
+    </div>
+</template>
+
+<script setup lang="ts">
+    import HeadProduct from '@/components/Display/headProduct/HeadProduct.vue';
+</script>
+
+<style lang="scss">
+    .container-card {
+        display: flex;
+        flex-direction: column;
+        width: 398px;
+        height: 447.64px;
+        padding: 16px;
+        gap: 18px;
+        background: #ffffff;
+        border-radius: 8px;
+    }
+    @media only screen and (min-width: 252px) and (max-width: 398px) {
+        .container {
+            width: auto;
+        }
+    }
+</style>
