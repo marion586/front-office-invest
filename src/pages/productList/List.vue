ba20be3df2cb6fc9c9c387b353b1ade4a5c8fd61
marion586 <marionmenye@gmail.com>
2022-09-30 17:44:53 +0300

    create head product

---
+++ src/pages/productList/List.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container-list-card">
+        <ProductCard />
+        <ProductCard />
+    </div>
+</template>
+
+<script setup lang="ts">
+    import ProductCard from '@/components/Display/productCard/ProductCard.vue';
+</script>
+
+<style lang="scss">
+    .container-list-card {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 20px;
+        justify-content: space-between;
+        padding: 20px;
+    }
+</style>
