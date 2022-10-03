9e544813e744f5fdefdfe964590d16492a30c94d
marion586 <marionmenye@gmail.com>
2022-10-03 15:52:25 +0300

    white headProduct test

--- src/pages/productList/cardProducts/CardProducts.test.js
+++ src/pages/productList/cardProducts/CardProducts.test.js
@@ -1,3 +1,9 @@
-describe('isd', () => {
-    
+import { shallowMount } from '@vue/test-utils';
+import { describe, expect, it } from 'vitest';
+import CardProducts from './CardProducts.vue';
+describe('CardProducts.vue', () => {
+    it('should render CardProducts.vue', () => {
+        const wrapper = shallowMount(CardProducts);
+        expect(wrapper.exists()).toBeTruthy();
+    });
 });
