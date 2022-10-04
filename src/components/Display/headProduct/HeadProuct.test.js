8bb6f5ff07e15aa76ef4e6610456090977b2a724
marion586 <marionmenye@gmail.com>
2022-10-04 17:18:58 +0300

    add head product test

--- src/components/Display/headProduct/HeadProuct.test.js
+++ src/components/Display/headProduct/HeadProuct.test.js
@@ -1,9 +1,25 @@
-import { shallowMount } from '@vue/test-utils';
+import { mount, shallowMount } from '@vue/test-utils';
 import { describe, expect, it } from 'vitest';
 import HeadProduct from './HeadProduct.vue';
 describe('HeadProduct.vue', () => {
     it('should render HeaderProduct.vue', () => {
         const wrapper = shallowMount(HeadProduct);
         expect(wrapper.exists()).toBeTruthy();
+    });
+    it('check props ', () => {
+        const text = 'menye';
+        const label = 'marion';
+
+        const headProduct = {
+            components: { HeadProduct },
+            template: ` <HeadProduct Text="${text}" label="${label}" />`,
+        };
+        const headProd = mount(headProduct);
+
+        expect(headProduct.components.HeadProduct).toBeDefined();
+        expect(headProd.getComponent(HeadProduct).props()).toEqual({
+            Text: text,
+            label: label,
+        });
     });
 });
