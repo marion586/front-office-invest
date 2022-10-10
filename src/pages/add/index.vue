300979aee6202901c6adb0563f71cd13a8803ba9
Manou-02 <mrakotoarivelo97@gmail.com>
2022-10-10 11:10:21 +0300

    Refactoring des statics data + changement du nom de classe css => OK

--- src/pages/add/index.vue
+++ src/pages/add/index.vue
@@ -8,84 +8,14 @@ import Photos from '@/pages/add/components/Photos/index.vue';
 import Price from '@/pages/add/components/Price/index.vue';
 import PropertyType from '@/pages/add/components/PropertyType/index.vue';
 import Steps from '@/components/Display/Steps/index.vue';
+import { data } from "@/pages/add/data";
 
 
 let activeComp = shallowRef<Array<object>>([PropertyType])
 
-interface SubMenu{
-      id : number,
-      label : string,
-      component ?: object
-}
 
-interface Menu {
-      id : number,
-      label : string,
-      icon : string,
-      subMenu ?: SubMenu[],
-      component ?: object
-}
 
-const menu = shallowRef<Array<Menu>>([
-      {
-            id : 0,
-            label : "Information de base",
-            icon : DefaultMenu,
-            subMenu : [
-                  {
-                        id : 0,
-                        label : "Type de bien",
-                        component : PropertyType
-                  },
-                  {
-                        id : 1,
-                        label : "Adresse",
-                        component : Adress
-                  },
-                  {
-                        id : 2,
-                        label : "Photos",
-                        component : Photos
-                  },
-                  {
-                        id : 3,
-                        label : "Prix",
-                        component : Price
-                  },
-            ]
-      },
-      {
-            id : 2,
-            label : "Information de base",
-            icon : DefaultMenu,
-            subMenu : [
-                  {
-                        id : 4,
-                        label : "Générale",
-                  },
-                  {
-                        id : 5,
-                        label : "intérieur",
-                  },
-                  {
-                        id : 6,
-                        label : "Caractéristique énergetique",
-                  },
-            ]
-      },
-      {
-            id : 3,
-            label : "Description de l'annonce",
-            icon : DefaultMenu,
-            component : PropertyType,
-      },
-      {
-            id : 4,
-            label : "Documents relatifs aux biens",
-            icon : DefaultMenu,
-            component : PropertyType,
-      }
-])
+const menu = shallowRef<Array<any>>(data)
 
 function changeComponent(comp : object){
       activeComp.value = [comp];
@@ -112,7 +42,7 @@ function changeComponent(comp : object){
                   @apply m-[12px] md:flex gap-[20px] md:gap-[10px]
             }
             &__content{
-                  @apply bg-[white] rounded-[8px] p-[24px] sm:w-[100%] lg:w-[893px];
+                  @apply bg-[white] rounded-[8px] p-[10px] md:p-[24px] sm:w-[100%] lg:w-[893px] mt-[10px] md:mt-[0px];
                   @media (min-width : 768px) {
                         min-height: calc(100vh - 80px);
                   }
